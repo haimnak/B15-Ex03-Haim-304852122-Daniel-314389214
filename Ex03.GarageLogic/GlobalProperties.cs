8430489396fef02bacafaa2fb54363a9bfe5dfa3
hihipriz <warsfear@gmail.com>
2015-05-30 14:39:00 +0300

    no message

---
+++ Ex03.GarageLogic/GlobalProperties.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    public class GlobalProperties
+    {
+        public const float maxAirPressureElectricCar = 31;
+        public const float maxAirPressureGasCar = 31;
+        public const float maxAirPressureElectricMotorCycle = 31;
+        public const float maxAirPressureGasMotorCycle = 34;
+        public const float maxAirPressureTruck = 25;
+
+
+        public const float maxEnergyElectricCar = (float)2.2;
+        public const float maxEnergyGasCar = 35;
+        public const float maxEnergyElectricMotorCycle = (float)1.2;
+        public const float maxEnergyGasMotorCycle = 8;
+        public const float maxEnergyTruck = 170;
+    }
+}
